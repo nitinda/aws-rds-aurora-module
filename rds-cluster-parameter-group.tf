447a69416bb6b061deb2d864969bfff3c591433b
Nitin Das <37865475+nitinda@users.noreply.github.com>
2022-03-30 11:31:27 +0100

    Create rds-cluster-parameter-group.tf

---
+++ rds-cluster-parameter-group.tf
@@ -0,0 +1,27 @@
+resource "aws_rds_cluster_parameter_group" "rds_cluster_parameter_group" {
+  count       = var.rds_cluster_parameter_group_name == null && var.rds_cluster_parameter_group_name_prefix == null ? 0 : 1
+  name        = var.rds_cluster_parameter_group_name
+  name_prefix = var.rds_cluster_parameter_group_name_prefix
+  family      = var.rds_cluster_parameter_group_family
+  description = var.rds_cluster_parameter_group_description
+
+  dynamic "parameter" {
+    for_each = var.rds_cluster_parameter_group_parameter == {} ? [] : [var.rds_cluster_parameter_group_parameter]
+    content {
+      apply_method = lookup(parameter.value, "apply_method", null)
+      name         = parameter.value.name
+      value        = parameter.value.value
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.prefix}-rds-cluster-parameter-group"
+    }
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
