37e7146ca586d44ee78913187393dfd11ab5bbeb
Nitin Das <37865475+nitinda@users.noreply.github.com>
2022-06-20 10:15:24 +0100

    Create db-parameter-group.tf

---
+++ db-parameter-group.tf
@@ -0,0 +1,28 @@
+resource "aws_db_parameter_group" "db_parameter_group" {
+  count       = var.db_parameter_group_name == null && var.db_parameter_group_name_prefix == null ? 0 : 1
+  name        = var.db_parameter_group_name
+  name_prefix = var.db_parameter_group_name_prefix
+  family      = var.db_parameter_group_family == null ? var.rds_cluster_parameter_group_family : var.db_parameter_group_family
+  description = var.db_parameter_group_description
+
+  dynamic "parameter" {
+    for_each = var.db_parameter_group_parameter
+    content {
+      apply_method = lookup(parameter.value, "apply_method", null)
+      name         = parameter.value.name
+      value        = parameter.value.value
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.prefix}-db-parameter-group"
+    }
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
