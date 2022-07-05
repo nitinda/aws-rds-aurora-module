ee5375d2ae8f2f27994a499e78aa480b11771252
Nitin Das <37865475+nitinda@users.noreply.github.com>
2022-07-05 01:11:38 +0100

    Update db-subnet-group.tf

--- db-subnet-group.tf
+++ db-subnet-group.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "db_subnet_group" {
-  count       = var.db_subnet_group_name == null && var.db_subnet_group_name_prefix == null ? 0 : 1
+  count       = var.db_subnet_group_name == null && var.db_subnet_group_name_prefix == null && var.rds_cluster_db_subnet_group_name == null ? 0 : 1
   name        = var.db_subnet_group_name
   name_prefix = var.db_subnet_group_name_prefix
   description = var.db_subnet_group_description
@@ -14,4 +14,8 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+locals {
+  rds_cluster_db_subnet_group_name = var.rds_cluster_db_subnet_group_name == null ? join("", aws_db_subnet_group.db_subnet_group.*.name) : var.rds_cluster_db_subnet_group_name
 }
