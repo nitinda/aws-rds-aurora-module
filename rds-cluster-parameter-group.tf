ea29bda26234b0f8c2a4b339801b740df05f4919
Nitin Das <37865475+nitinda@users.noreply.github.com>
2022-07-05 01:12:13 +0100

    Update rds-cluster-parameter-group.tf

--- rds-cluster-parameter-group.tf
+++ rds-cluster-parameter-group.tf
@@ -1,12 +1,12 @@
 resource "aws_rds_cluster_parameter_group" "rds_cluster_parameter_group" {
-  count       = var.rds_cluster_parameter_group_name == null && var.rds_cluster_parameter_group_name_prefix == null ? 0 : 1
+  count       = var.rds_cluster_parameter_group_name == null && var.rds_cluster_parameter_group_name_prefix == null && var.rds_cluster_db_parameter_group_name == null ? 0 : 1
   name        = var.rds_cluster_parameter_group_name
   name_prefix = var.rds_cluster_parameter_group_name_prefix
   family      = var.rds_cluster_parameter_group_family
   description = var.rds_cluster_parameter_group_description
 
   dynamic "parameter" {
-    for_each = var.rds_cluster_parameter_group_parameter == {} ? [] : [var.rds_cluster_parameter_group_parameter]
+    for_each = var.rds_cluster_parameter_group_parameter
     content {
       apply_method = lookup(parameter.value, "apply_method", null)
       name         = parameter.value.name
@@ -24,4 +24,8 @@ resource "aws_rds_cluster_parameter_group" "rds_cluster_parameter_group" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+locals {
+  rds_cluster_db_parameter_group_name = var.rds_cluster_db_parameter_group_name == null ? join("", aws_rds_cluster_parameter_group.rds_cluster_parameter_group.*.name) : var.rds_cluster_db_parameter_group_name
 }
