1c0b69322d413bb64a42521d889f17b819a4f7ae
Nitin Das <37865475+nitinda@users.noreply.github.com>
2022-07-05 01:11:02 +0100

    Update db-parameter-group.tf

--- db-parameter-group.tf
+++ db-parameter-group.tf
@@ -1,5 +1,5 @@
 resource "aws_db_parameter_group" "db_parameter_group" {
-  count       = var.db_parameter_group_name == null && var.db_parameter_group_name_prefix == null ? 0 : 1
+  count       = var.db_parameter_group_name == null && var.db_parameter_group_name_prefix == null && var.rds_cluster_instance_db_parameter_group_name == null ? 0 : 1
   name        = var.db_parameter_group_name
   name_prefix = var.db_parameter_group_name_prefix
   family      = var.db_parameter_group_family == null ? var.rds_cluster_parameter_group_family : var.db_parameter_group_family
@@ -24,5 +24,8 @@ resource "aws_db_parameter_group" "db_parameter_group" {
   lifecycle {
     create_before_destroy = true
   }
+}
 
+locals {
+  rds_cluster_instance_db_parameter_group_name = var.rds_cluster_instance_db_parameter_group_name == null ? join("", aws_db_parameter_group.db_parameter_group.*.name) : var.rds_cluster_instance_db_parameter_group_name
 }
